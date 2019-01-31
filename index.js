6efe13792020b488d5b3fa94b9be787810cdc6b6
camilletan39 <camille_merlin_tan@dlsu.edu.ph>
2019-01-31 14:12:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var map;
+var makersLayer = new L.LayerGroup();
 var code="1X3IE_mFJTnNhYSuBZYaHL7_2G7EgWrQdSAM5Eewnwwg"
 
 document.addEventListener('DOMContentLoaded',function(){
-  map = L.map('map').setView([14, 121], 7);
+  map = L.map('map').setView([14.5, 120.9], 9);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
@@ -33,9 +34,32 @@ document.addEventListener('DOMContentLoaded',function(){
         L.marker([place.latitude, place.longitude])
           .addTo(map)
           .bindPopup(place.name)
+          markersLayer.addLayer(marker); 
       }
     },
     simpleSheet: true 
   })
-  
+  markersLayer.addTo(map)
 })
+
+function updatePoints() {
+ // change points
+      Tabletop.init({ 
+    key: code,
+    callback: function(sheet, tabletop){ 
+      markersLayer.clearLayers();
+      for (var i in sheet){
+        var place = sheet[i];
+        var marker= L.marker([place.latitude, place.longitude])
+          .bindPopup(place.name);
+        markersLayer.addLayer(marker); 
+      }
+    },
+    simpleSheet: true 
+    
+  })
+markersLayer.addTo(map);
+ map.invalidateSize();
+ setTimeout(function(){ updatePoints(); }, 10000);}
+
+window.addEventListener("load", function() { updatePoints(); });
