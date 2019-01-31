33f0c4cd687507a7f6020fa7622180e1ce5fe350
camilletan39 <camille_merlin_tan@dlsu.edu.ph>
2019-01-31 14:43:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded',function(){
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
   
+  var controlLayers = L.control.layers( null, null, {
+  position: "topright",
+  collapsed: false
+  }).addTo(map);
+
+  
    var light = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
   }); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
