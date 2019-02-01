b066a8fc7657d06d5c69ac1be041e847ace6e12a
camilletan39 <camille_merlin_tan@dlsu.edu.ph>
2019-02-01 11:33:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var markersLayer = new L.LayerGroup();
 var code = "1X3IE_mFJTnNhYSuBZYaHL7_2G7EgWrQdSAM5Eewnwwg"
 
 document.addEventListener('DOMContentLoaded',function(){
-  map = L.map('map').setView([14.5, 120.9], 7);
+  map = L.map('map').setView([14.5, 120.9], 9);
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
