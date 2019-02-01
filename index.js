333836123fc1e9ab5d8769a2c2cc5d86005da6b3
camilletan39 <camille_merlin_tan@dlsu.edu.ph>
2019-02-01 13:45:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var map;
+var startdate;
+var enddate;
 var markersLayer = new L.LayerGroup();
 var code = "1X3IE_mFJTnNhYSuBZYaHL7_2G7EgWrQdSAM5Eewnwwg"
 
@@ -14,13 +16,21 @@ document.addEventListener('DOMContentLoaded',function(){
       markersLayer.clearLayers();
       for (var i in sheet){
         var place = sheet[i];
-var marker= L.marker([place.latitude, place.longitude]).bindPopup("<center>"+place.date +"<br>"+ place.value);
+        if (startdate==null){
+          var marker= L.marker([place.latitude, place.longitude]).bindPopup(place.date + " <br>"+place.value);
         markersLayer.addLayer(marker); 
+        }else{
+          if(newDate(place.date) >= newDate(startdate) && newDate(place.date) <= newDate(enddate)){
+          var marker= L.marker([place.latitude, place.longitude]).bindPopup(place.date + " <br>"+place.value);
+        markersLayer.addLayer(marker); 
+          }
+        }
+
       }
     },
     
     simpleSheet: true 
-     
+    
   })
   markersLayer.addTo(map)
 })
@@ -33,9 +43,16 @@ function updatePoints() {
       markersLayer.clearLayers();
       for (var i in sheet){
         var place = sheet[i];
-        var marker= L.marker([place.latitude, place.longitude])
-          .bindPopup("<center>"+place.date +"<br>"+ place.value);
+        
+        if (startdate==null){
+          var marker= L.marker([place.latitude, place.longitude]).bindPopup("<center>"+place.date + "<br>"+place.value);
         markersLayer.addLayer(marker); 
+        }else{
+          if((new Date(place.date)) >= startdate && (new Date(place.date)) <= enddate){
+          var marker= L.marker([place.latitude, place.longitude]).bindPopup("<center>"+place.date + "<br>"+place.value);
+        markersLayer.addLayer(marker); 
+          }
+        }
       }
     },
     simpleSheet: true 
@@ -46,3 +63,22 @@ markersLayer.addTo(map);
  setTimeout(function(){ updatePoints(); }, 10000);}
 
 window.addEventListener("load", function() { updatePoints(); });
+
+$(function() {
+  $('input[name="datetimes"]').daterangepicker({
+    timePicker: true,
+    timePicker24Hour:true,
+    autoUpdateInput: false,
+    locale: {
+      format: 'YYYY-MM-DD HH:mm:ss',
+      cancelLabel: 'Clear'
+    }
+  });
+});
+
+
+$('input[name="datetimes"]').on('apply.daterangepicker', function(ev, picker) {
+  $(this).val(picker.startDate.format('YYYY-MM-DD HH:mm:ss') + ' - ' + picker.endDate.format('YYYY-MM-DD HH:mm:ss'));
+  startdate = new Date(picker.startDate.format('YYYY-MM-DD HH:mm:ss'));
+  enddate = new Date(picker.endDate.format('YYYY-MM-DD HH:mm:ss'));
+});
